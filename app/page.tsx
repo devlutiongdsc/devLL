e0600687b6a8d23bec751ec2169c8bf646bf0d48
jeet1703 <prabhjeetdec03@gmail.com>
2024-03-31 01:41:19 +0530

    almost final changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,19 @@ import Image from "next/image";
 import Waitlist from "@/components/Waitlist"
 import Hero from "@/components/Hero"
 import { BackgroundBeams } from "@/components/beamBg";
+import Herobg from "@/components/Herobg";
+
 
 
 export default function Home() {
   return (
-    <main className="">
-      
-      <div>
+    <main className="h-full w-full">
+      <div className="flex flex-col h-[90%] gap-20">
         {/* <Hero/> */}
-        <Waitlist />
-        {/* <BackgroundBeams /> */}
+      
+        <Herobg/>
+       
+        
       </div>
 
     </main>
